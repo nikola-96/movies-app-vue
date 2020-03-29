159454f4196743cbff2bd58191692eb4f6659aac
Nikola <nikolaviki@hotmail.com>
2020-03-29 13:03:17 +0200

    Zadatak 2

---
+++ src/services/MovieService.js
@@ -0,0 +1,16 @@
+
+import HTTP from "./BaseService";
+
+class MoviesService {
+    async getAll() {
+        const response = await HTTP.get('/movies');
+        return response.data;
+    }
+
+}
+
+
+
+const moviesService = new MoviesService();
+
+export default moviesService;
